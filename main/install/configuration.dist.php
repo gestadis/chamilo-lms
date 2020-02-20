9ecd46a2125b829fd848efe4cc30367b3139ad50
Julio <gugli100@gmail.com>
2020-02-20 11:31:29 +0100

    Admin: add config setting "whoisonline_only_for_admin" BT#16758

--- main/install/configuration.dist.php
+++ main/install/configuration.dist.php
@@ -950,9 +950,11 @@ VALUES (2, 13, 'session_courses_read_only_mode', 'Lock Course In Session', 1, 1,
 // Set to true to disable the new personal data page inside the social network menu
 // $_configuration['disable_gdpr'] = true;
 
-// GDPR requires users to be informed of the Data Protection Officer name and contact point
-// These can only be defined here for now, but will be moved to web settings in the future.
-// Name of the person or organization that is responsible for the treatment of personal info
+// GDPR requires users to be informed of the Data Protection Officer name and
+// contact point. These can only be defined here for now, but will be moved to
+// web settings in the future.
+// Name of the person or organization that is responsible for the treatment of
+// personal info
 //$_configuration['data_protection_officer_name'] = '';
 // A description of the role of the DP Officer in this context
 //$_configuration['data_protection_officer_role'] = '';
@@ -1364,8 +1366,24 @@ ALTER TABLE notification_event ADD COLUMN event_id INT NULL;
 // ALTER TABLE sys_announcement ADD COLUMN promotion_id INT DEFAULT 0;
 //$_configuration['allow_careers_in_global_announcements'] = false;
 
-// Allow start/end dates in "My courses" page (user_portal.php)
-//$_configuration['allow_session_dates_in_user_portal'] = false;
+// Hide start/end dates in "My courses" page (user_portal.php)
+//$_configuration['hide_session_dates_in_user_portal'] = false;
+
+// Catalog search settings visibility
+//$_configuration['catalog_settings'] = ['sessions' => ['by_title' => true, 'by_date' => true, 'by_tag' => true, 'show_session_info' => true, 'show_session_date' => true]];
+
+// Enable learning paths with only one SCO item to use the score returned by
+// the SCO as an indicator of progress of the whole learning path
+// $_configuration['lp_score_as_progress_enable'] = false;
+
+// Use this link as the "Forgot password?" link instead of the default. This setting should be transformed into a hook for plugins at a later time
+//$_configuration['pass_reminder_custom_link'] = '';
+
+// In Scorm comunication use the username instead of the user_id
+//$_configuration['scorm_api_username_as_student_id'] = false;
+
+// Show online user only to Administrators
+//$_configuration['whoisonline_only_for_admin'] = false;
 
 // KEEP THIS AT THE END
 // -------- Custom DB changes
